bdea8e861683560e42a0b15b7ee0f1ceb1ca5760
gyuminv2 <inamemin3@gmail.com>
2025-01-10 14:24:10 +0900

    feat : be

---
+++ backend/jimechu/src/main/java/jiandgyu/jimechu/JimechuApplication.java
@@ -0,0 +1,13 @@
+package jiandgyu.jimechu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JimechuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JimechuApplication.class, args);
+	}
+
+}
---
+++ backend/jimechu/src/test/java/jiandgyu/jimechu/JimechuApplicationTests.java
@@ -0,0 +1,13 @@
+package jiandgyu.jimechu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JimechuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
