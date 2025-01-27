fea0a2441dfc1f0e391182ab81fd5478aa69fbc4
gyuminv2 <inamemin3@gmail.com>
2025-01-27 13:47:47 +0900

    TEST : member 로그인 성공/실패(인증) 테스트코드  작성

--- backend/jimechu/src/test/java/jiandgyu/jimechu/unit/service/MemberServiceTest.java
+++ backend/jimechu/src/test/java/jiandgyu/jimechu/unit/service/MemberServiceTest.java
@@ -1,21 +1,22 @@
 package jiandgyu.jimechu.unit.service;
 
+import jiandgyu.jimechu.config.security.JwtTokenProvider;
+import jiandgyu.jimechu.config.security.TokenInfo;
 import jiandgyu.jimechu.domain.Member;
 import jiandgyu.jimechu.domain.MemberRole;
-import jiandgyu.jimechu.dto.MemberCreateDTO;
+import jiandgyu.jimechu.dto.LoginRequestDTO;
 import jiandgyu.jimechu.repository.MemberRepository;
 import jiandgyu.jimechu.repository.MemberRoleRepository;
 import jiandgyu.jimechu.service.MemberService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -34,14 +35,18 @@ class MemberServiceTest {
     @Mock private MemberRepository memberRepository;
     @Mock private MemberRoleRepository memberRoleRepository;
     @Mock private BCryptPasswordEncoder passwordEncoder;
+    @Mock private AuthenticationManagerBuilder authenticationManagerBuilder;
+    @Mock private Authentication mockAuthentication;
+    @Mock private JwtTokenProvider jwtTokenProvider;
     @InjectMocks private MemberService memberService;
+
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
     }
 
     @Test
-    public void 회원가입() throws Exception {
+    public void 회원가입() {
         // given
         Member member = getMember();
 
@@ -88,11 +93,67 @@ class MemberServiceTest {
         verify(memberRepository, never()).save(any(Member.class));
     }
 
+    @Test
+    public void 로그인_성공() {
+        // given
+        String nickname = "지나";
+        String password = "지나123";
+        LoginRequestDTO requestDTO = new LoginRequestDTO();
+        requestDTO.setNickname(nickname);
+        requestDTO.setPassword(password);
+
+        TokenInfo mockTokenInfo = new TokenInfo("Bearer", "mockAccessToken");
+
+        // Mock 설정
+        when(authenticationManagerBuilder.getObject()).thenReturn(authentication -> mockAuthentication);
+        when(mockAuthentication.getPrincipal()).thenReturn(nickname);
+        when(jwtTokenProvider.createToken(mockAuthentication)).thenReturn(mockTokenInfo);
+
+        // when
+        TokenInfo tokenInfo = memberService.login(requestDTO);
+
+        // then
+        assertNotNull(tokenInfo, "TokenInfo should not be null");
+        assertEquals("Bearer", tokenInfo.getGrantType());
+        assertEquals("mockAccessToken", tokenInfo.getAccessToken());
+    }
+
+    @Test
+    public void 로그인_실패() {
+        // given
+        String nickname = "invalid지나";
+        String password = "invalid지나123";
+        LoginRequestDTO requestDTO = new LoginRequestDTO();
+        requestDTO.setNickname(nickname);
+        requestDTO.setPassword(password);
+
+        // Mock : Authentication 실패
+        when(authenticationManagerBuilder.getObject())
+                .thenReturn(authentication -> {
+                    throw new RuntimeException("Authentication failed");
+                });
+
+        // when
+        RuntimeException exception = Assertions.assertThrows(
+                RuntimeException.class, () -> {
+                    memberService.login(requestDTO);
+                });
+
+        // then
+        assertEquals("Authentication failed", exception.getMessage());
+        verify(authenticationManagerBuilder, times(1)).getObject();
+        verify(jwtTokenProvider, never()).createToken(any(Authentication.class));
+    }
+
+
+
+
     private static Member getMember() {
         Member member = new Member();
         member.setNickname("지나");
         member.setPassword("지나123");
         return member;
     }
+
 
 }
